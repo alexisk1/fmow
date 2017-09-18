00cc17193056c3b610fc7c77372df11b56a94af3
alexisk1 <alexiskdr2@gmail.com>
2017-09-18 22:25:48 +0100

    List bucket with requestor pays header

---
+++ list_bucket_req_pays.py
@@ -0,0 +1,17 @@
+import boto
+from boto.s3.key import Key
+import boto.s3.connection
+
+Bucketname = 'fmow-rgb'       
+
+
+conn = boto.connect_s3()
+
+
+bucket = conn.get_bucket(Bucketname, headers={'x-amz-request-payer': 'requester'})
+
+
+for key in bucket.list(headers={'x-amz-request-payer': 'requester'}):
+   print( key.name,
+          key.size,
+          key.last_modified)
