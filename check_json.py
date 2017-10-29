48a5a81f9b95fbb41504ff30ed509b7e6955b4cc
a.kanter <a.kanter@robotcaresystems.com>
2017-10-29 14:18:48 +0000

    start

--- check_json.py
+++ check_json.py
@@ -2,7 +2,7 @@ import re
 import json
 from pprint import pprint
 
-with open('./manifest.json') as data_file:    
+with open('./manifest-full.json') as data_file:    
     data = json.load(data_file)
 
 train =0
@@ -15,6 +15,9 @@ val_classes=[]
 val_classes_keys=[]
 test=0
 test_classes= []
+test_classes_keys =[]
+print(data[1:10])
+
 for i in range(len(data)):
       if 'train' in data[i]['key'][0:5]:
           train= train+1
@@ -22,15 +25,18 @@ for i in range(len(data)):
           m = pattern.search(data[i]['key'])
           if m.group('ending') == 'json':
              train_json = train_json +1
-          if m.group('ending') == 'jpg':
+          if m.group('ending') == 'tif':
              train_jpg = train_jpg +1
           if m.group('path') not in tr_classes:
              tr_classes.append(m.group('path'))
              n=0
              for x in range(10):
-                while( m.group('ending') != 'jpg'):
+                try:
+                 while( m.group('ending') != 'tif'):
                     n=n+1
                     m = pattern.search(data[i+n]['key'])
+                except:
+                  print(data[i+n]['key'],  m.group('ending')) 
                 tr_classes_keys.append(data[i+n]['key'])
                 n=n+1
                 m = pattern.search(data[i+n]['key'])
@@ -41,6 +47,7 @@ for i in range(len(data)):
           if m!=None: 
             if m.group('path') not in test_classes:
              test_classes.append(m.group('path'))
+             test_classes_keys.append(data[i]['key'])
       if 'val' in data[i]['key'][0:3]:
           val= val+1
           pattern = re.compile(r"val/(?P<path>[a-zA-Z0-9-]+)/.*\.(?P<ending>[a-z]+)")
@@ -51,7 +58,7 @@ for i in range(len(data)):
              n=0
              for x in range(10):
                 try:
-                   while( m.group('ending') != 'jpg' and i+n<len(data) ):
+                   while( m.group('ending') != 'tif' and i+n<len(data) ):
                        n=n+1
                        m = pattern.search(data[i+n]['key'])
                    val_classes_keys.append(data[i+n]['key'])
@@ -76,20 +83,24 @@ val_classes = sorted(val_classes)
 print("Train keys",len(tr_classes_keys))
 print("Val jeys",len(val_classes_keys))
 
-
+input()
 import boto3
 
 s3client = boto3.client('s3')
 
 
 
-for i in val_classes_keys:
+for i in tr_classes_keys:
           pattern = re.compile(r".*/(?P<name>[a-zA-Z0-9_]+).*$")
           pattern2 = re.compile(r"(?P<name>[a-zA-Z0-9_/]+).*$")
           m = pattern.search(i)
           m2 = pattern2.search(i)
           fname = m.group('name')
           pall = m2.group('name')
-          s3client.download_file('fmow-rgb', pall+'.json' , 'val/'+fname+'.json',{'RequestPayer':'requester'})
-          s3client.download_file('fmow-rgb', pall+'.jpg'  , 'val/'+fname+'.jpg',{'RequestPayer':'requester'})
+          print(pall, fname)
+          try:
+            s3client.download_file('fmow-full', pall+'.json' , 'train_fl/'+fname+'.json',{'RequestPayer':'requester'})
+            s3client.download_file('fmow-full', pall+'.tif'  , 'train_fl/'+fname+'.tif',{'RequestPayer':'requester'})
+          except:
+            print("not:",pall)
 
